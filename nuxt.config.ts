521db65cc6ac9552643c9083840366d112846ebd
Maltys <maltys@live.com.au>
2026-08-26 21:37:16 +1000

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,30 @@
 export default defineNuxtConfig({
-  compatibilityDate: "2026-08-01",
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/supabase"],
-  css: ["~/assets/css/main.css"],
+  compatibilityDate: '2026-08-01',
+
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/supabase'
+  ],
+
+  css: ['~/assets/css/main.css'],
+
   runtimeConfig: {
-    spotifyClientId: process.env.SPOTIFY_CLIENT_ID,
-    spotifyClientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-    spotifyRedirectUri: process.env.SPOTIFY_REDIRECT_URI,
-    supabaseServiceRoleKey: process.env.SUPABASE_SERVICE_ROLE_KEY,
+    spotifyClientId: '',
+    spotifyClientSecret: '',
+    spotifyRedirectUri: '',
+    supabaseServiceRoleKey: '',
+    supabaseUrl: '',
+
     public: {
-      appUrl:
-        process.env.NUXT_PUBLIC_APP_URL || "https://kcjukebox.netlify.app/",
-    },
+      appUrl: ''
+    }
   },
+
   supabase: {
     redirectOptions: {
-      login: "/",
-      callback: "/confirm",
-      exclude: ["/*"],
-    },
-  },
-});
+      login: '/',
+      callback: '/confirm',
+      exclude: ['/*']
+    }
+  }
+})
